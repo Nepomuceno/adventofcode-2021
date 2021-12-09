a548a80b225932a4a0cf06b11c351e0f31130faf
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2021-12-09 22:52:48 +0000

    day 9

--- day9/part1.py
+++ day9/part1.py
@@ -6,20 +6,40 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-file = "day8/input.txt"
+file = "day9/input.txt"
 
-displays = [[]]
-number_of_uniques = 0
+def get_neighbours(i, j, points):
+    neighbours = []
+    height = len(points)
+    width = len(points[0])
+    if i > 0:
+        neighbours.append(int(points[i - 1][j]))
+    if i < height - 1:
+        neighbours.append(int(points[i + 1][j]))
+    if j > 0:
+        neighbours.append(int(points[i][j - 1]))
+    if j < width - 1:
+        neighbours.append(int(points[i][j + 1]))
+    return neighbours
+
+points = [[]]
+danger_level = 0
 with open(file) as f:
-    displays = [ x.split("|")[1].split() for x in f.readlines()]
+    points = [ list(x.strip()) for x in f.readlines()]
+
+height = len(points)
+width = len(points[0])
 
 
-for digits in displays:
-    for digit in digits:
-        if len(digit) != 5 and len(digit) != 6:
-            number_of_uniques += 1
+for i in range(height):
+    for j in range(width):
+        neighbours = get_neighbours(i, j, points)
+        log.debug(f"{i},{j} neighbours: {neighbours}")
+        if int(points[i][j]) < min(neighbours):
+            log.info(f"{i},{j} is a low point of value {points[i][j]}")
+            danger_level += (int(points[i][j]) + 1)
+log.info(f"Danger level: {danger_level}")
 
-log.info(f"Number of unique digits: {number_of_uniques}")
 
 
 
--- day9/part2.py
+++ day9/part2.py
@@ -1,119 +1,143 @@
 import io
 import logging
-from typing import List
-import pprint
+import os
+from typing import List, Tuple
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
-pp = pprint.PrettyPrinter(indent=2)
 
-file = "day8/input.txt"
-all_wires = list("abcdefg")
 
-def load_basic_digits(all_10: List[str]):
-    digits = {}
-    for digit in all_10:
-        if len(digit) == 2:
-            digits["1"] = digit
-        if len(digit) == 4:
-            digits["4"] = digit
-        if len(digit) == 3:
-            digits["7"] = digit
-        if len(digit) == 7:
-            digits["8"] = digit
-    return digits
+file = "day9/input.txt"
 
-def discover_a(discovered_wires: dict, discovered_digits: dict):
-    seven = discovered_digits["7"]
-    one = discovered_digits["1"]
-    seven = [value for value in seven if value not in one]
-    discovered_wires["a"] = seven[0]
-    return discovered_wires
+def local_low(i, j, points, basins) -> bool:
+    height = len(points)
+    width = len(points[0])
+    response = False
+    if i > 0:
+        if (i-1, j) not in basins:
+            response = True
+            if int(points[i-1][j]) <= int(points[i][j]):
+                return False 
+    if i < height - 1:
+        if (i+1,j) not in basins:
+            response = True
+            if int(points[i+1][j]) <= int(points[i][j]):
+                return False
+    if j > 0:
+        if (i,j-1) not in basins:
+            response = True
+            if int(points[i][j-1]) <= int(points[i][j]):
+                return False
+    if j < width - 1:
+        if (i,j+1) not in basins:
+            response = True
+            if int(points[i][j+1]) <= int(points[i][j]):
+                return False
+    if response == True and int(points[i][j]) == 9:
+        log.error("Found a local low at %s,%s" % (i,j))
+    return response
 
-def discover_9_and_e(discovered_wires: dict, discovered_digits: dict, all_10: List[str]):
-    four = discovered_digits["4"]
-    for digit in all_10:
-        if len(digit) != 6:
-            continue
-        intersection = [value for value in digit if value in four]
-        if len(intersection) == 4:
-            discovered_digits["9"] = digit
-            e_wire = [value for value in all_wires if value not in digit]
-            discovered_wires["e"] = e_wire[0]
-    return discovered_wires, discovered_digits
+def get_basin(i: int, j: int, points:List[List[int]], basins:List) -> List:
+    height = len(points)
+    width = len(points[0])
+    if i > 0:
+        if (i-1, j) not in basins:
+            if local_low(i-1,j,points,basins):
+                basins.append((i-1,j))
+                basins = get_basin(i-1,j,points,basins)
+    if i < height - 1:
+        if (i+1,j) not in basins:
+            if local_low(i+1,j,points,basins):
+                basins.append((i+1,j))
+                basins = get_basin(i+1,j,points,basins)
+    if j > 0:
+        if (i,j-1) not in basins:
+            if local_low(i,j-1,points,basins):
+                basins.append((i,j-1))
+                basins = get_basin(i,j-1,points,basins)
+    if j < width - 1:
+        if (i,j+1) not in basins:
+            if local_low(i,j+1,points,basins):
+                basins.append((i,j+1))
+                basins = get_basin(i,j+1,points,basins)
+    return basins
 
-def discover_3(discovered_digits: dict, discovered_wires: dict, all_10: List[str]):
-    one = discovered_digits["1"]
-    for digit in all_10:
-        if len(digit) != 5:
-            continue
-        intersection = [value for value in digit if value in one]
-        if len(intersection) == 2:
-            discovered_digits["3"] = digit
-    return discovered_digits
+def get_lowest_neighbours_coordinates(i, j, points):
+    value = int(points[i][j])
+    lower_value = (float("inf"), (0,0))
+    if i > 0:
+        if int(points[i - 1][j]) < value and int(points[i - 1][j]) < lower_value[0]:
+            lower_value = (int(points[i - 1][j]), (i - 1, j))
+    if i < height - 1:
+        if int(points[i + 1][j]) < value and int(points[i + 1][j]) < lower_value[0]:
+            lower_value = (int(points[i + 1][j]), (i + 1, j))
+    if j > 0:
+        if int(points[i][j - 1]) < value and int(points[i][j - 1]) < lower_value[0]:
+            lower_value = (int(points[i][j - 1]), (i, j - 1))
+    if j < width - 1:
+        if int(points[i][j + 1]) < value and int(points[i][j + 1]) < lower_value[0]:
+            lower_value = (int(points[i][j + 1]), (i, j + 1))
+    return lower_value[1]
 
-def discover_6_and_0(discovered_wires: dict, discovered_digits: dict, all_10: List[str]):
-    remaining_digits = [value for value in all_10 if value not in discovered_digits.values()]
-    one = discovered_digits["1"]
-    for digit in remaining_digits:
-        if len(digit) != 6:
-            continue
-        intersection = [value for value in digit if value in one]
-        if len(intersection) == 2:
-            discovered_digits["0"] = digit
-        else:
-            discovered_digits["6"] = digit
-    six = discovered_digits["6"]
-    zero = discovered_digits["0"]
-    nine = discovered_digits["9"]
-    seven = discovered_digits["7"]
-    tree = discovered_digits["3"]
-    four = discovered_digits["4"]
-    c_wire = [value for value in zero if value not in six] 
-    discovered_wires["c"] = c_wire[0]
-    d_wire = [value for value in nine if value not in zero]
-    discovered_wires["d"] = d_wire[0]
-    a_c = [discovered_wires["a"], discovered_wires["c"]]
-    f_wire = [value for value in seven if value not in a_c]
-    discovered_wires["f"] = f_wire[0]
-    a_c_d_f = [discovered_wires["a"], discovered_wires["c"], discovered_wires["d"], discovered_wires["f"]]
-    g_wire = [value for value in tree if value not in a_c_d_f]
-    discovered_wires["g"] = g_wire[0]
-    b_wire = [value for value in four if value not in a_c_d_f]
-    discovered_wires["b"] = b_wire[0]
-    discovered_digits["5"] = sorted([discovered_wires["a"], discovered_wires["b"], discovered_wires["d"], discovered_wires["f"], discovered_wires["g"]])
-    remaining_digits = [value for value in all_10 if value not in discovered_digits.values()]
-    discovered_digits["2"] = remaining_digits[0]
-    return discovered_wires, discovered_digits
+# Calculate distance between two points in a Matrix including diagonals
+def caculate_distance(point_a: Tuple[int,int], point_b: Tuple[int,int]):
+    return abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
 
-def calculate_display(display: str):
-    all_10 =[sorted(x) for x in display.split("|")[0].split()]
-    output = [sorted(x) for x in display.split("|")[1].split()]
-    discovered_wires = {}
-    discovered_digits = load_basic_digits(all_10)
-    discovered_wires = discover_a(discovered_wires, discovered_digits)
-    discovered_wires, discovered_digits = discover_9_and_e(discovered_wires, discovered_digits, all_10)
-    discovered_digits = discover_3(discovered_digits, discovered_wires, all_10)
-    discovered_wires, discovered_digits = discover_6_and_0(discovered_wires, discovered_digits, all_10)
-    # log.debug(pp.pprint(discovered_wires))
-    # log.debug(pp.pprint(discovered_digits))
-    content = ""
-    for value in output:        
-        number_value = list(discovered_digits.keys())[list(discovered_digits.values()).index(value)]
-        content += number_value
-    return content
-displays = []
-total = 0
+# Get the closest low point to the given point
+# def get_closest_low(i, j, lowest_points:dict):
+#     distances = [(caculate_distance((i,j), point),point) for point in lowest_points.keys()]
+#     log.info("Distances: %s" % distances) 
+#     min_distance = min(distances)
+#     log.info("Min distance: %s, %s" % min_distance)
+#     return min_distance[1]
+
+def get_neighbours(i, j, points):
+    neighbours = []
+    height = len(points)
+    width = len(points[0])
+    if i > 0:
+        neighbours.append(int(points[i - 1][j]))
+    if i < height - 1:
+        neighbours.append(int(points[i + 1][j]))
+    if j > 0:
+        neighbours.append(int(points[i][j - 1]))
+    if j < width - 1:
+        neighbours.append(int(points[i][j + 1]))
+    return neighbours
+
+points = [[]]
+low_points = {}
+danger_level = 0
 with open(file) as f:
-    displays = [ x for x in f.readlines()]
+    points = [ list(x.strip()) for x in f.readlines()]
 
-for display in displays:
-    content = calculate_display(display)
-    total += int(content)
-    log.info(int(content))
-
-log.info(total)
+height = len(points)
+width = len(points[0])
+basins_legth_collection = []
 
 
+for i in range(height):
+    for j in range(width):
+        neighbours = get_neighbours(i, j, points)
+        log.debug(f"{i},{j} neighbours: {neighbours}")
+        if int(points[i][j]) < min(neighbours):
+            log.debug(f"{i},{j} is a low point of value {points[i][j]}")
+            danger_level += (int(points[i][j]) + 1)
+            low_points[(i,j)] = []
 
+for i in range(height):
+    for j in range(width):
+        if points[i][j] != '9':
+            if (i,j) in low_points.keys():
+                low_points[(i,j)].append((i,j))
+                continue
+            lowest_neigbour = get_lowest_neighbours_coordinates(i,j,points)
+            while lowest_neigbour not in low_points:
+                lowest_neigbour = get_lowest_neighbours_coordinates(lowest_neigbour[0],lowest_neigbour[1],points)
+            log.info(f"{i},{j} is a low point of value {lowest_neigbour}")
+            low_points[lowest_neigbour].append((i,j))
 
+log.debug(f"{low_points}")
+basins = sorted([len(basins) for basins in low_points.values()])
+log.debug(f"Basins: {basins}")
+log.info(f"Danger level: { basins[-1] * basins[-2] * basins[-3] }")
